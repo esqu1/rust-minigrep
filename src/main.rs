3c07973f4ab9f1e14238e2b86abb142057ac8021
esqu1 <brandbest1@gmail.com>
2019-07-04 12:30:19 -0700

    add basic arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query: &String = &args[1];
+    let filename: &String = &args[2];
+
+    println!("Command: {}", query);
+    println!("Filename: {}", filename);
 }
